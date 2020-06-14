f88ecd0878cf5fd2ff7aa6b851b2b4c8d0810210
Łukasz <61921500+LK-Herman@users.noreply.github.com>
2020-06-14 21:47:27 +0200

    Laboratorium 6 Testy jednostkowe

---
+++ Lab6_1/Silnia.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab6_1
+{
+    public class Silnia
+    {
+        public int Oblicz(int s)
+        {
+            if (s < 2)
+                return 1;
+            else
+            {
+                return s * Oblicz(s - 1);
+            }
+        }
+    }
+}
---
+++ Lab6_1/UnitTest1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Lab6_1;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SilniaTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Silnia_0()
+        {
+            Silnia silnia = new Silnia();
+            Assert.AreEqual(silnia.Oblicz(0), 1);
+        }
+        [TestMethod]
+        public void Silnia_1()
+        {
+            Silnia silnia = new Silnia();
+            Assert.AreEqual(silnia.Oblicz(1), 1);
+        }
+        [TestMethod]
+        public void Silnia_5()
+        {
+            Silnia silnia = new Silnia();
+            Assert.AreEqual(silnia.Oblicz(5), 120);
+        }
+
+    }
+}
